af7dfe3e59a0fb40e73d5f902da4662f7cd16f80
lomayd <jinho10167@gmail.com>
2022-08-29 17:04:49 +0900

    Initial Commit

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,38 @@
+package hello.hellospring;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+@Configuration
+public class SpringConfig {
+    //private final DataSource dataSource;
+    //private final EntityManager em;
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+    /*
+    @Bean
+    public MemberRepository memberRepository() {
+    // return new MemoryMemberRepository();
+    // return new JdbcMemberRepository(dataSource);
+    // return new JdbcTemplateMemberRepository(dataSource);
+    // return new JpaMemberRepository(em);
+    }
+    */
+}
